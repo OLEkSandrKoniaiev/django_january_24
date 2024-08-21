6df8ef6472578988063d0cd22e959b2a2aab859b
OLEkSandrKoniaiev <128646916+OLEkSandrKoniaiev@users.noreply.github.com>
2024-08-21 14:10:42 +0300

    lesson10

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -20,6 +20,7 @@ AUTH_USER_MODEL = 'users.UserModel'
 
 # Application definition
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.staticfiles',
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
     'apps.auto_parks',
     'apps.users',
     'apps.auth',
+    'apps.chat',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +64,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'configs.wsgi.application'
+ASGI_APPLICATION = 'configs.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
